8d95bd58c26b76c4e8580ed79972e2746ab87418
Rhian Castro <rhi.castro@gmail.com>
2020-08-12 22:09:27 -0300

    deleted some files and fixed others

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,31 @@
 require('dotenv').config();
 
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const path = require('path');
 const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const mongoose = require('mongoose');
+
+const path = require('path');
 const { PORT } = process.env.HTTP_PORT;
 const port = PORT || 3000;
 const app = express();
-const BooksRoutes = require('./routes/BooksRoutes');
 const UserRoutes = require('./routes/UserRoutes');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-/**
- * Database setup
- */
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
 });
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'assets')));
-
-app.use('/books/', BooksRoutes)
 app.use('/users', UserRoutes)
 
-app.listen(3000);
+app.listen(port);
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  registration: { type: Number, require: true },
+  email: {type: String, require: true},
   name: { type: String, require: true },
-  turma: { type: String, require: true },
   senha: { type: Number, require: true },
   token: { type: String },
   role: { type: String, default: 'user', require: true },
