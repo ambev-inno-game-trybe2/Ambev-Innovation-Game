9ed8dc2de26626af39a55c26b871fad70d6e0273
Hfreitas <hfreitas86@gmail.com>
2020-08-13 11:07:52 -0300

    implementação inicial da tela de preferencias do usuario

--- frontend/src/pages/UserPreferences.jsx
+++ frontend/src/pages/UserPreferences.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import IngredientCard from '../components/IngredientCard';
 
+const text = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque voluptates id iste nemo distinctio error recusandae perspiciatis molestias minus possimus iure vel aliquam, blanditiis provident voluptatem, voluptatum consequuntur voluptas laudantium?`;
+
 export default function UserPreferences({ ingredients }) {
-  return <section>
-    <h1>Escolha 5 ingredientes</h1>
-    {ingredients.map((ingredient) => (
-      <IngredientCard key={ingredient.name} ingredient={ingredient} />
-    ))}
-    <button type="button">Finalizar</button>
-  </section>;
+  const [showModal, setShowModal] = useState(true)
+  return (
+    <section>
+      <InfoModal text={text} onHide={() => setShowModal(!showModal)} />
+      <h1>Escolha 5 ingredientes</h1>
+      {ingredients.map((ingredient) => (
+        <IngredientCard key={ingredient.name} ingredient={ingredient} />
+      ))}
+      <button type="button">Pular</button>
+      <button type="button">Finalizar</button>
+    </section>
+  );
 }
 
 UserPreferences.propTypes = {
   ingredients: PropTypes.arrayOf(PropTypes.obj).isRequired,
-}
+};
