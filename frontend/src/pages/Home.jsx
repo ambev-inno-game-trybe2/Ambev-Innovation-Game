4e0068640ef33768c3f5c0e7f811d9d4541bc245
Hfreitas <hfreitas86@gmail.com>
2020-08-13 12:24:04 -0300

    implementando state ao componente

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useForm from '../hooks/useForm';
 import FormField from '../components/FormField';
 import appLogo from '../assets/logo2.png';
 import facebookLogin from '../assets/facebook-login-icon-19.jpg';
 import googleLogin from '../assets/btn_google_signin_light_normal_web.png';
 
+const initialState = {
+  userName: '',
+  password: '',
+};
+
 export default function Home() {
+  const { clearForm, handleChange } = useForm(initialState);
   return (
     <section>
       <img src={appLogo} alt="I'm Thirsty Logo" />
-      <form>
-        <FormField placeholder="Nome" type="text" name="userName" required />
-        <FormField placeholder="Senha" type="text" name="password" required />
+      <form onSubmit={() => clearForm()}>
+        <FormField
+          placeholder="Nome"
+          type="text"
+          name="userName"
+          required
+          onChange={(event) => handleChange(event)}
+        />
+        <FormField
+          placeholder="Senha"
+          type="text"
+          name="password"
+          required
+          onChange={(event) => handleChange(event)}
+        />
         <button type="button">Login</button>
         <Link to="/signup">
           <button type="button">Cadastre-se</button>
