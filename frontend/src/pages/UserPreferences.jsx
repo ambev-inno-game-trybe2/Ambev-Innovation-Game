11b159cc8864dcdc11d635227e747ebc518d3042
Hfreitas <hfreitas86@gmail.com>
2020-08-14 12:00:22 -0300

    modal ok

--- frontend/src/pages/UserPreferences.jsx
+++ frontend/src/pages/UserPreferences.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import IngredientCard from '../components/IngredientCard';
+import InfoModal from '../components/InfoModal';
 
 const text = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque voluptates id iste nemo distinctio error recusandae perspiciatis molestias minus possimus iure vel aliquam, blanditiis provident voluptatem, voluptatum consequuntur voluptas laudantium?`;
 
 export default function UserPreferences({ ingredients }) {
-  const [showModal, setShowModal] = useState(true)
+  const [showModal, setShowModal] = useState(true);
   return (
     <section>
-      <InfoModal text={text} onHide={() => setShowModal(!showModal)} />
+      <InfoModal
+        show={showModal}
+        text={text}
+        onHide={() => setShowModal(!showModal)}
+      />
       <h1>Escolha 5 ingredientes</h1>
       {ingredients.map((ingredient) => (
         <IngredientCard key={ingredient.name} ingredient={ingredient} />
